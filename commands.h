993ca98084f4db4741d84dc8d21565c51f9c68f2
coinerwh <coinerwh@gmail.com>
2021-04-26 00:08:11 -0700

    started system command handler

--- commands.h
+++ commands.h
@@ -1,6 +1,6 @@
 // Function declarations for built-in command function
 
-void exit_cmd();
+void exit_cmd(char *status, struct userCommand *currCommand, char *userInput);
 void cd_cmd(struct userCommand *currCommand);
 void status_cmd(char *status);
 void system_cmd(struct userCommand *currCommand, char *status);
