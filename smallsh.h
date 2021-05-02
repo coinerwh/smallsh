296e77590097194ea8e64f0c32a19c6cfeea2d2f
coinerwh <coinerwh@gmail.com>
2021-05-01 18:30:35 -0700

    fixed global var duplicate declare

--- smallsh.h
+++ smallsh.h
@@ -3,12 +3,13 @@
 #include "childpid_struct.h"
 #include "command_struct.h"
 
-int foregroundOnly;
-
 void cleanupStruct(struct userCommand* currStruct);
 void commandHandler(struct userCommand* currCommand, char *status, char *userInput, struct childpidStruct *childPids);
 void printCommandStruct(struct userCommand* currCommand);
 void smallsh();
 
 // foreground switch declaration
+#ifndef FOREGROUNDONLY
+#define FOREGROUNDONLY
 extern int foregroundOnly;
+#endif
